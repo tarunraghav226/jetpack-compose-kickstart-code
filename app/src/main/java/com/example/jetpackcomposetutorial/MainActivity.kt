8d30fc021a737d2a979bcfa3cfcc56d8b40023f9
tarun <tarun.1@pw.live>
2022-08-24 17:55:40 +0530

    Added updated view

--- app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposetutorial/MainActivity.kt
@@ -30,24 +30,35 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.jetpackcomposetutorial.models.UserDetails
+import com.example.jetpackcomposetutorial.navigation.CustomNavHost
+import com.example.jetpackcomposetutorial.ui.composables.BottomNavigationView
 import com.example.jetpackcomposetutorial.ui.composables.UserListComponents
 import com.example.jetpackcomposetutorial.ui.theme.JetpackComposeTutorialTheme
 import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val navController = rememberNavController()
             JetpackComposeTutorialTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background,
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background,
+//                ) {
+////                    ScrollableComponent()
+////                    ShowList()
+//                    UserListComponents(users = getListOfUsers())
+//                }
+                Scaffold(
+                    bottomBar = {BottomNavigationView(navController = navController)}
                 ) {
-//                    ScrollableComponent()
-//                    ShowList()
-                    UserListComponents(users = getListOfUsers())
+                    CustomNavHost(navController = navController)
                 }
             }
         }
